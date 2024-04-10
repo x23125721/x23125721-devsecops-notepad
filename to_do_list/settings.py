5812cdbef8f03dbd1d57ea3c079cf99ae061d067
x23125721 <x23125721@student.ncirl.ie>
2024-04-10 06:19:45 +0100

    added few more files

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -132,6 +132,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.0/ref/settings/#default-auto-field
 
